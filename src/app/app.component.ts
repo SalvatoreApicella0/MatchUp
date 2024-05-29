9540f8f0fb5bee57e0e5e6245423763ff28dab39
SalvatoreApicella0 <dev@salvatoreapicella.com>
2024-05-29 12:07:02 +0200

    aggiunta el codice per upload e download e rename dei file per il carosello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,50 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'matchup';
+  uploadedFile: File | null = null;
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(private elementRef: ElementRef) {
+    this.fileInput = elementRef.nativeElement.querySelector('#fileInput');
+  }
+  
+
+  openFileBrowser() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.uploadedFile = event.target.files[0];
+  
+    const formData = new FormData();
+    formData.append('file', file);
+    
+    
+  }
+
+  downloadFile() {
+    if (this.uploadedFile) {
+      const blob = new Blob([this.uploadedFile], { type: this.uploadedFile.type });
+      const url = window.URL.createObjectURL(blob);
+      
+      // Creare un link temporaneo e simularne il clic per avviare il download
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = this.uploadedFile.name;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      // Rilascia l'URL creato per evitare memory leaks
+      window.URL.revokeObjectURL(url);
+    }
+  }
+
 }
+
+
+
+  
+
