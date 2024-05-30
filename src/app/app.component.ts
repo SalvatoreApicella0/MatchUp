884dab8da609ad27595e11d5a5d18768bbe0cf54
SalvatoreApicella0 <dev@salvatoreapicella.com>
2024-05-30 20:19:42 +0200

    update per inferfacciarsi col server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component, ViewChild, ElementRef  } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'matchup';
   uploadedFile: File | null = null;
+  responseFile: File | null = null;
   loading = false;
 
   template_cv = [
@@ -27,7 +29,6 @@ export class AppComponent {
   ];
   currentIndex_cv_carousel = 3; // L'indice dell'immagine centrale iniziale
 
-
   templateScelto = this.template_cv[this.currentIndex_cv_carousel].slice(8, -4);
 
   @ViewChild('fileInput') fileInput: ElementRef;
@@ -35,65 +36,75 @@ export class AppComponent {
   constructor(private elementRef: ElementRef) {
     this.fileInput = elementRef.nativeElement.querySelector('#fileInput');
   }
-  
 
   openFileBrowser() {
-    this.loading = true;
     this.uploadedFile = null;
     this.fileInput.nativeElement.click();
   }
 
   onFileSelected(event: any) {
-    
     const file: File = event.target.files[0];
-    this.uploadedFile = event.target.files[0];
-  
-    const formData = new FormData();
-    formData.append('file', file);
-    
-    //this.templateScelto questa è la variabile che ha all'interno il
-    //nome del template scelto(tranne estensione), 
-    //che o può essere usato come ID stesso, o convertito in ID per la post
+    this.uploadedFile = file;
 
-    let x = 0;
-    for (let i = 0; i < 10000000000; i++) {
-      x = 2 * 2;
+    // Chiamata del metodo di caricamento
+    this.uploadFile();
+  }
+
+  async uploadFile() {
+    if (this.uploadedFile) {
+      this.toggleLoadingIndicator(true);
+
+      const formData = new FormData();
+      formData.append('file', this.uploadedFile);
+      formData.append('text', 'Your text here');
+
+      try {
+        const response = await axios.post('http://localhost:5100/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        console.log('Success:', response.data);
+        this.responseFile = response.data;
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        this.toggleLoadingIndicator(false);
+      }
     }
+  }
 
-    this.loading = false;
-
-    
+  toggleLoadingIndicator(show: boolean) {
+    this.loading = show;
   }
 
   downloadFile() {
-    if (this.uploadedFile) {
-      const blob = new Blob([this.uploadedFile], { type: this.uploadedFile.type });
+    if (this.responseFile) {
+      const blob = new Blob([this.responseFile], { type: this.responseFile.type });
       const url = window.URL.createObjectURL(blob);
-  
+
       // Creare un link temporaneo e simularne il clic per avviare il download
       const link = document.createElement('a');
       link.href = url;
-      link.download = this.templateScelto + ".png"; // Utilizza il nuovo nome del file
+      link.download = 'Output.docx';
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-  
+
       // Rilascia l'URL creato per evitare memory leaks
       window.URL.revokeObjectURL(url);
     }
   }
-  
 
-  
   scrollLeft() {
     this.currentIndex_cv_carousel = (this.currentIndex_cv_carousel - 1 + this.template_cv.length) % this.template_cv.length;
-    this.templateScelto = this.template_cv[this.currentIndex_cv_carousel].slice(0, -4);
+    this.templateScelto = this.template_cv[this.currentIndex_cv_carousel];
     this.updateVisibleImages();
   }
 
   scrollRight() {
     this.currentIndex_cv_carousel = (this.currentIndex_cv_carousel + 1) % this.template_cv.length;
-    this.templateScelto = this.template_cv[this.currentIndex_cv_carousel].slice(8, -4);
+    this.templateScelto = this.template_cv[this.currentIndex_cv_carousel];
     this.updateVisibleImages();
   }
 
@@ -116,10 +127,8 @@ export class AppComponent {
       }
     }
   }
-  
 
   ngAfterViewInit() {
     this.updateVisibleImages();
   }
-
 }
